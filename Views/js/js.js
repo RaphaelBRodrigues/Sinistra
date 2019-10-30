2a83809a1c7144904339f867185996700a3605bc
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-10-30 13:39:05 -0300

    Carrosel add nav fixed

--- Views/js/js.js
+++ Views/js/js.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  var index=1;
+function carrosel(){
+  if (index>3) {
+    index=1;
+  }
+  $(".fotos-foto").attr("src","Views/src/banda"+index+".jpg");
+  index++;
 
-$(".main-img").attr("src","Views/src/banda2.jpg");
+
+}
+setInterval(function(){carrosel()},2500);
 });
